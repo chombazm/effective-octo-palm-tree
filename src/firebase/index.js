7d3b48cd23c7c7780cce943e8108b3f0eb0def47
Chomba <dev.chomba@gmail.com>
2023-02-05 16:55:29 +0200

    Initialise project

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyD7qfoagTnjOA-XILHCpBbvsr73xg0jSdk',
+  authDomain: 'carlisting-3a36d.firebaseapp.com',
+  projectId: 'carlisting-3a36d',
+  storageBucket: 'carlisting-3a36d.appspot.com',
+  messagingSenderId: '913956663707',
+  appId: '1:913956663707:web:c57029582dc3a037a1f13e'
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
